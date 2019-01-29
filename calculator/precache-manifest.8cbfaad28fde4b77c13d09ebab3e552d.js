7d2cb41f1ae082882b195c74efb62ddc15a99e1c
Cody Zuschlag <cody.zuschlag@gmail.com>
2019-01-29 08:47:51 +0100

    add calculator

---
+++ calculator/precache-manifest.8cbfaad28fde4b77c13d09ebab3e552d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5f5139b78fafdf93bd1",
+    "url": "/calculator/static/js/runtime~main.b5f5139b.js"
+  },
+  {
+    "revision": "332a65b92e869cb1f1f2",
+    "url": "/calculator/static/js/main.332a65b9.chunk.js"
+  },
+  {
+    "revision": "726d2dd55a5d7fcfb2fe",
+    "url": "/calculator/static/js/1.726d2dd5.chunk.js"
+  },
+  {
+    "revision": "726d2dd55a5d7fcfb2fe",
+    "url": "/calculator/static/css/1.2e79f83b.chunk.css"
+  },
+  {
+    "revision": "2e8d2c3481e776113130a293e0d218f8",
+    "url": "/calculator/index.html"
+  }
+];
